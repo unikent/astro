41390b8f3426d6e0c31ca9ae78d19a23c50d8807
Jack <jb2034@kent.ac.uk>
2017-02-10 15:04:50 +0000

    Add sourcemaps because error messages are annoying atm.

--- webpack.mix.js
+++ webpack.mix.js
@@ -27,4 +27,5 @@ mix
 	)
 	.js('resources/assets/js/app.js', 'public/js')
 	.sass('resources/assets/sass/app.scss', 'public/css')
-	.extract(['vue', 'jquery', 'axios', 'tether', 'bootstrap', 'element-ui']);
+	.extract(['vue', 'jquery', 'axios', 'tether', 'bootstrap', 'element-ui'])
+	.sourceMaps();
