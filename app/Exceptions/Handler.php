3361ccc02347bf8348ea844f417c6da15c7be361
Tom Gillett <tom@wewritetheweb.org>
2017-04-07 14:24:38 +0100

    Configured global error handling for API friendly errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class Handler extends ExceptionHandler
 {
@@ -17,7 +18,7 @@ class Handler extends ExceptionHandler
 		\Illuminate\Auth\AuthenticationException::class,
 		\Illuminate\Auth\Access\AuthorizationException::class,
 		\Symfony\Component\HttpKernel\Exception\HttpException::class,
-		\Illuminate\Database\Eloquent\ModelNotFoundException::class,
+		// \Illuminate\Database\Eloquent\ModelNotFoundException::class,
 		\Illuminate\Session\TokenMismatchException::class,
 		\Illuminate\Validation\ValidationException::class,
 	];
@@ -44,7 +45,39 @@ class Handler extends ExceptionHandler
 	 */
 	public function render($request, Exception $exception)
 	{
-		return parent::render($request, $exception);
+		if($request->route()){
+			$action = $request->route()->getAction();
+			$prefix = $action['prefix'];
+		} else {
+			$prefix = null;
+		}
+
+		switch($prefix){
+			case 'api/v1':
+				$classname = substr(strrchr(get_class($exception), '\\'), 1);;
+
+				switch(get_class($exception)){
+					case 'App\Exceptions\DefinitionNotFoundException':
+					case 'Illuminate\Database\Eloquent\ModelNotFoundException':
+						return response()->json([ 'errors' => [[ 'message' => 'Not found', 'reason' => $classname ]] ], 404);
+						break;
+
+					default:
+						return response()->json([ 'errors' => [[
+							'message' => $classname,
+							'reason' => $classname,
+							'trace' => $exception->getTraceAsString(),
+						]] ], 500);
+						break;
+				}
+
+				break;
+
+			default:
+				return parent::render($request, $exception);
+				break;
+		}
+
 	}
 
 	/**
