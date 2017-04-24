1c6d2181e73b166dd675d90eb777994d2549901c
Jack <jb2034@kent.ac.uk>
2017-04-24 12:35:51 +0100

    Remove old event bus code from bootstrap file.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,7 +1,4 @@
 import './redactor';
-import Vue from 'vue';
-
-window.eventBus = (window.self === window.top ? new Vue() : window.top.eventBus);
 
 window.axios.defaults.headers.common = {
 	'X-Requested-With': 'XMLHttpRequest',
