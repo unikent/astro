75640383c2355ea6c9efff9c1082eacdd2ba47ef
Jack <jb2034@kent.ac.uk>
2017-05-30 13:39:28 +0100

    Make sure snackbar is shared between iframe and main window.

--- resources/assets/js/plugins/snackbar.js
+++ resources/assets/js/plugins/snackbar.js
@@ -1,18 +1,26 @@
 import SnackBar from '../components/SnackBar';
+import { isIframe, win } from 'classes/helpers';
 
 /* global document */
 
 export default (Vue, el = false) => {
-	const snackbar = new (Vue.extend(SnackBar));
-	snackbar.vm = snackbar.$mount();
+	let snack;
+
+	if(!isIframe) {
+		const snackbar = new (Vue.extend(SnackBar));
+		snackbar.vm = snackbar.$mount();
+		win.snackbar = snack = snackbar.vm;
+		(el ? el : document.body).appendChild(snackbar.vm.$el);
+	}
+	else {
+		snack = win.top.snackbar;
+	}
 
 	Object.defineProperties(Vue.prototype, {
 		$snackbar: {
 			get() {
-				return snackbar.vm;
+				return snack;
 			}
 		}
 	});
-
-	(el ? el : document.body).appendChild(snackbar.vm.$el);
 };
