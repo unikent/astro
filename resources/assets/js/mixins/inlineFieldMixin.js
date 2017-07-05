af73e07049a171dac22d3ac70f97d7566210b4f8
Jack <jb2034@kent.ac.uk>
2017-07-05 10:22:55 +0100

    Update inline edit code (directive for flagging fields as inline and a mixin for actually making the field inline editable).

--- resources/assets/js/mixins/inlineFieldMixin.js
+++ resources/assets/js/mixins/inlineFieldMixin.js
@@ -1,43 +1,31 @@
 import { mapMutations } from 'vuex';
 
-/* global $, window */
-
 export default {
 
 	methods: {
 		...mapMutations([
 			'updateFieldValue'
-		]),
-
-		markAsInline(name) {
-			console.log(`Added ${this.index} - ${name}`)
-		}
+		])
 	},
 
 	created() {
-		// keep these private and untracked
 		this.internalChange = false;
 		this.inlineFields = {};
 	},
 
 	mounted() {
-		const self = this;
-
 		Object.keys(this.inlineFields).forEach(fieldName => {
-			$(this.inlineFields[fieldName]).redactor({
-				focus: true,
-				toolbarExternal: window.parent.document.querySelector('#toolbar'),
-				callbacks: {
-					change() {
-						console.log(this);
-						self.updateFieldValue({
-							index: self.index,
-							name: fieldName,
-							value: this.code.get()
-						});
-						self.internalChange = true;
-					}
-				}
+			// TODO: replace contenteditable with another instance of scribe
+			this.inlineFields[fieldName].setAttribute('contenteditable', true);
+
+			this.inlineFields[fieldName].addEventListener('input', (e) => {
+
+				this.updateFieldValue({
+					name: fieldName,
+					value: e.target.innerHTML
+				});
+
+				this.internalChange = true;
 			});
 		});
 	}
