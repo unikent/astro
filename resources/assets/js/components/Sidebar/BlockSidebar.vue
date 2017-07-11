c40b02d50e01c10546e893b9f252ffff4285b127
Jack <jb2034@kent.ac.uk>
2017-07-11 11:20:57 +0100

    Add new sidebar components for displaying new menu sections, within our single sidebar.
    Block addition will soon be added inline, rather than dragging them in from the sidebar (this change breaks the dragging functionality).

--- resources/assets/js/components/Sidebar/BlockSidebar.vue
+++ resources/assets/js/components/Sidebar/BlockSidebar.vue
@@ -1,44 +1,17 @@
 <template>
-	<div style="
-		height: 100%;
-		background-color: #f5f7fa;
-	">
-		<block-list></block-list>
-		<block-options class="block-options" :class="{'options-visible' : mode === 'edit'}"></block-options>
-	</div>
+	<block-options :title="title"></block-options>
 </template>
 
 <script>
-import BlockOptions from './BlockOptions';
-import BlockList from './BlockList';
-import { mapState } from 'vuex';
+import BlockOptions from 'components/BlockOptions';
 
 export default {
 	name: 'BlockSidebar',
 
+	props: ['title'],
+
 	components: {
-		BlockOptions,
-		BlockList
-	},
-
-	computed: {
-		...mapState({
-			currentBlockDefinition: state => state.definition.currentBlockDefinition
-		}),
-
-		mode() {
-			return this.currentBlockDefinition ? 'edit' : 'list';
-		}
-	},
-
-	methods: {
-		editBlock() {
-			this.mode = 'edit';
-		},
-
-		exitEditBlock() {
-			this.mode = 'list';
-		}
+		BlockOptions
 	}
 };
 </script>
