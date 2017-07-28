ac7e4bfc3d3d186ce1f78f7b32ae1eefb66e6fb5
Jack <jb2034@kent.ac.uk>
2017-07-28 15:36:59 +0100

    Use v-show to hide bottom bar rather than styles (is it's used elsewhere).

--- resources/assets/js/components/BlockOptions.vue
+++ resources/assets/js/components/BlockOptions.vue
@@ -67,7 +67,8 @@
 
 	</div>
 
-	<div class="b-bottom-bar">
+	<!-- hide until we know what we're doing with validation -->
+	<div class="b-bottom-bar" v-show="false">
 		<el-button :plain="true" type="danger" @click="deleteThisBlock">Remove</el-button>
 		<el-button @click="submitForm('block_fields')">Validate</el-button>
 	</div>
