af804d731c42155704d5f3b490d4f06e7bf4bed4
Sam Yapp <sam@samyapp.com>
2017-08-02 10:27:57 +0000

    Change routes to use plurals instead of singular, eg. /site/1/tree -> /sites/1/tree

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,29 @@
 <?php
 
 Route::group([ 'prefix' => 'v1', 'namespace' => 'v1' ], function(){
-	Route::get('block/definitions', 'BlockController@definitions');
-	Route::get('block/{block_definition}/definition', 'BlockController@definition');
-	Route::get('block/{block_definition}/blocks', 'BlockController@blocks');
+	Route::get('blocks/definitions', 'BlockController@definitions');
+	Route::get('blocks/{block_definition}/definition', 'BlockController@definition');
+	Route::get('blocks/{block_definition}/blocks', 'BlockController@blocks');
 
-	Route::get('layout/definitions', 'LayoutController@definitions');
-	Route::get('layout/{layout_definition}/definition', 'LayoutController@definition');
+	Route::get('layouts/definitions', 'LayoutController@definitions');
+	Route::get('layouts/{layout_definition}/definition', 'LayoutController@definition');
 
 	// Media does not use resource routing as {media} gets pluralized to {medium}, resulting in dragons.
 	Route::get('media', 'MediaController@index');
 	Route::post('media', 'MediaController@store');
 	Route::delete('media/{media}', 'MediaController@destroy');
 
-	Route::resource('page', 'PageContentController', [ 'except' => [ 'index', 'create', 'edit' ]]);
-	Route::post('page/{page}/publish', 'PageContentController@publish');
-	Route::post('page/{page}/publish-tree', 'PageContentController@publishTree');
-	Route::post('page/{page}/revert', 'PageContentController@revert');
-	Route::delete('page/{page}/force', 'PageContentController@forceDestroy');
+	Route::resource('pages', 'PageContentController', [ 'except' => [ 'index', 'create', 'edit' ]]);
+	Route::post('pages/{page}/publish', 'PageContentController@publish');
+	Route::post('pages/{page}/publish-tree', 'PageContentController@publishTree');
+	Route::post('pages/{page}/revert', 'PageContentController@revert');
+	Route::delete('pages/{page}/force', 'PageContentController@forceDestroy');
 
-	Route::get('region/definitions', 'RegionController@definitions');
-	Route::get('region/{region_definition}/definition', 'RegionController@definition');
+	Route::get('regions/definitions', 'RegionController@definitions');
+	Route::get('regions/{region_definition}/definition', 'RegionController@definition');
 
-	Route::get('route/resolve', 'PageController@resolve');
+	Route::get('routes/resolve', 'PageController@resolve');
 
-	Route::resource('site', 'SiteController', [ 'only' => [ 'index', 'show', 'store', 'update', 'destroy' ]]);
-	Route::get('site/{site}/tree', 'SiteController@tree');
+	Route::resource('sites', 'SiteController', [ 'only' => [ 'index', 'show', 'store', 'update', 'destroy' ]]);
+	Route::get('sites/{site}/tree', 'SiteController@tree');
 });
