c7d4bca1648f66950b721cb898babfee28cc1e48
Jack <jb2034@kent.ac.uk>
2017-08-03 11:20:43 +0100

    Add media module to our vuex store.

--- resources/assets/js/store/modules/media.js
+++ resources/assets/js/store/modules/media.js
@@ -1,14 +1,55 @@
-const state = {};
+const state = {
+	mediaPicker: {
+		visible: false,
+		fieldPath: null,
+		mediaType: 'media'
+	},
+	mediaOverlayVisible: false,
+	mediaItem: {}
+};
+
+const mutations = {
+	showMediaPicker(state) {
+		state.mediaPicker.visible = true;
+	},
+
+	hideMediaPicker(state) {
+		state.mediaPicker.visible = false;
+	},
+
+	setMediaType(state, type) {
+		state.mediaPicker.mediaType = type;
+	},
+
+	updateMediafieldPath(state, path) {
+		state.mediaPicker.fieldPath = path;
+	},
+
+	setMediaOverlayVisibility(state, visible) {
+		state.mediaOverlayVisible = visible;
+	},
+
+	setMediaItem(state, value) {
+		state.mediaItem = value;
+	}
+};
+
+const actions = {
+	showMediaOverlay({ commit }, item) {
+		commit('setMediaItem', item);
+		commit('setMediaOverlayVisibility', true);
+	},
+
+	hideMediaOverlay({ commit }) {
+		commit('setMediaOverlayVisibility', false);
+	}
+};
 
 const getters = {};
 
-const actions = {};
-
-const mutations = {};
-
 export default {
 	state,
-	getters,
 	actions,
-	mutations
+	mutations,
+	getters
 };
