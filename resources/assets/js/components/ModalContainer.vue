96a27e5dabd355aea6fc1a5338cb83f4483320ba
Jack <jb2034@kent.ac.uk>
2017-08-03 11:08:52 +0100

    Create a modal container component to store all of our modal windows (to simplify adding new modals and ensure they are at the top-most level of the editor).

---
+++ resources/assets/js/components/ModalContainer.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+	<block-picker />
+	<publish-modal />
+</div>
+</template>
+
+<script>
+import BlockPicker from 'components/BlockPicker';
+import PublishModal from 'components/PublishModal';
+
+export default {
+
+	name: 'modal-container',
+
+	components: {
+		BlockPicker,
+		PublishModal
+	}
+
+};
+</script>
