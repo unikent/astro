934593433ba966e715230c943bb871db3b49194f
Matthew Bull <m.bull@kent.ac.uk>
2017-09-15 16:13:53 +0100

    removed one of the hardcoded example links

--- resources/assets/js/components/Sidebar/Errors.vue
+++ resources/assets/js/components/Sidebar/Errors.vue
@@ -6,9 +6,6 @@
 			<el-tag type="danger"><i class="el-icon-warning"></i><a href="#" class="validation-errors__link">Call-to-action</a></el-tag>
 		</li>
 		<li class="validation-errors__item">
-			<el-tag type="success"><i class="el-icon-circle-check"></i><a href="#" class="validation-errors__link">Site search</a></el-tag>
-		</li>
-		<li class="validation-errors__item">
 			<el-tag type="success"><i class="el-icon-circle-check"></i><a href="#" class="validation-errors__link">Key links</a></el-tag>
 		</li>
 	</ul>
