97e68f5ea15d93cf0b64bfa6a1ce27963b2dd44b
Christian Cable <c.f.cable@kent.ac.uk>
2017-09-19 09:45:42 +0100

    Block Form now has a computer prop with the current validation status

--- resources/assets/js/components/BlockForm.vue
+++ resources/assets/js/components/BlockForm.vue
@@ -1,12 +1,33 @@
 <script>
+/**
+custom form component for use with the block editor which reports its current validation 
+state to the vuex store
+
+inherits from the standard Element Form http://element.eleme.io/#/en-US/component/form
+**/
 import Vue from 'vue';
 import { Form } from 'element-ui';
 
 export default Vue.component('el-block-form', {
 	extends: Form,
+	
+	computed: {
+		valid: function() {
+			let invalidElements = this.fields.filter(function (value) {
+				return value.validateState == 'error';
+			});
+			return invalidElements.length == 0;
+			// return invalidElements;
+		}
+	},
+
+	watch: {
+
+	},
+
 	data() {
 		return {
-			extendedMode: ''
+			
 		}
 	},
 })
