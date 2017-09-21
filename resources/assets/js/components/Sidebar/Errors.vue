e939b83293c6537aecd5d7cfef42424ce1ff0afb
Christian Cable <c.f.cable@kent.ac.uk>
2017-09-21 18:14:27 +0100

    Basic styling for blocks in errors panel - needs review by UI

--- resources/assets/js/components/Sidebar/Errors.vue
+++ resources/assets/js/components/Sidebar/Errors.vue
@@ -1,18 +1,30 @@
 <template>
-<div>
-	<back-bar :title="title" />
-	<ul class="validation-errors">
-		<li class="validation-errors__item">
-			<i class="el-icon-warning"></i><a href="#" class="validation-errors__link">Call-to-action</a>
-		</li>
-		<li class="validation-errors__item">
-			<i class="el-icon-warning"></i><a href="#" class="validation-errors__link">Key links</a>
-		</li>
-	</ul>
-</div>
+	<div>
+		<back-bar :title="title" />
+		<template v-if="blocks" v-for="region in blocks">
+			<!-- we could put the region title here when we use more than one region -->			
+			<ul class="validation-errors" v-if="region">
+				<template  v-for="block in region">
+					<template v-if="errors.indexOf(block.id) === -1">
+						<li class="validation-errors__item success">
+							<i class="el-icon-circle-check"></i>
+							<a href="#" class="validation-errors__link">{{ block.definition_name }}</a>
+						</li>
+					</template>
+					<template v-else>
+						<li class="validation-errors__item warning">
+							<i class="el-icon-warning"></i>
+							<a href="#" class="validation-errors__link">{{ block.definition_name }}</a>
+						</li>
+					</template>
+				</template>
+			</ul>		
+		</template>
+	</div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import BackBar from 'components/BackBar';
 
 export default {
@@ -20,8 +32,22 @@ export default {
 
 	props: ['title'],
 
+	computed: {
+		errors() {
+			return this.$store.state.page.invalidBlocks;
+		}, 
+
+		blocks() {
+			return this.$store.state.page.pageData.blocks;
+		},
+
+		region() {
+			return this.$store.state.page.currentRegion;	
+		}
+	},
+
 	components: {
 		BackBar
-	}
+	},
 };
 </script>
