08511d98142585bd0695217c82cf00d5f76b6196
Sam Yapp <sam@samyapp.com>
2017-09-26 10:08:54 +0000

    Limit to only insert code after first finding of <body>

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::get('/draft/{page}', function(Request $request, Page $page) {
     // add in preview bar
     $original_html = $astro->renderRoute($page->site->host, $page->site->path . $page->path, $api, $engine, $locator, Page::STATE_DRAFT);
     $preview_bar = file_get_contents(resource_path('views/components/preview-bar.html'));
-    return preg_replace('/(<body[^>]*>)/is', '$1' . $preview_bar, $original_html);
+    return preg_replace('/(<body[^>]*>)/is', '$1' . $preview_bar, $original_html, 1);
 })
 ->where('route', '(.*?)/?')
 ->middleware('auth');
