309253ffc9998862aa8d01f98b8274d52a3b76f1
Sam Yapp <sam@samyapp.com>
2017-09-26 10:03:47 +0000

    Inject preview bar to stick at the top of every preview page.
    A regex attempts to insert the content of the preview template html directly below the opening body tag.
    
    The preview bar has position fixed to stick it at the top of the page, a fixed height, and an empty div with the
    same height after it to push the top of the page content down below it.

--- routes/web.php
+++ routes/web.php
@@ -43,9 +43,8 @@ Route::get('/draft/{page}', function(Request $request, Page $page) {
 
     // add in preview bar
     $original_html = $astro->renderRoute($page->site->host, $page->site->path . $page->path, $api, $engine, $locator, Page::STATE_DRAFT);
-    $preview_bar = ''; // TODO read file in from preview-bar.html
-	
-    return str_replace('<body>', '<body>' . $preview_bar, $original_html);
+    $preview_bar = file_get_contents(resource_path('views/components/preview-bar.html'));
+    return preg_replace('/(<body[^>]*>)/is', '$1' . $preview_bar, $original_html);
 })
 ->where('route', '(.*?)/?')
 ->middleware('auth');
