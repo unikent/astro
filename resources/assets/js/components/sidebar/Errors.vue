6d0405164751357289acc433a4b913e31e5ff75e
Matthew Bull <m.bull@kent.ac.uk>
2017-09-27 07:58:21 +0100

    added jump links for block errors
    - added jump links so users can jump to the error block
    - hid valid blocks from the error sidebar

--- resources/assets/js/components/sidebar/Errors.vue
+++ resources/assets/js/components/sidebar/Errors.vue
@@ -2,23 +2,17 @@
 	<div>
 		<back-bar :title="title" />
 		<template v-if="blocks" v-for="region in blocks">
-			<!-- we could put the region title here when we use more than one region -->			
+			<!-- we could put the region title here when we use more than one region -->
 			<ul class="validation-errors" v-if="region">
-				<template  v-for="block in region">
-					<template v-if="errors.indexOf(block.id) === -1">
-						<li class="validation-errors__item success">
-							<i class="el-icon-circle-check"></i>
-							<a href="#" class="validation-errors__link">{{ block.definition_name }}</a>
-						</li>
-					</template>
-					<template v-else>
+				<template v-for="(block, key) in region">
+					<template v-if="errors.indexOf(block.id) !== -1">
 						<li class="validation-errors__item warning">
 							<i class="el-icon-warning"></i>
-							<a href="#" class="validation-errors__link">{{ block.definition_name }}</a>
+							<a href="#" @click="scrollTo(key)" class="validation-errors__link">{{ label(block.definition_name + "-v" + block.definition_version) }}</a>
 						</li>
 					</template>
 				</template>
-			</ul>		
+			</ul>
 		</template>
 	</div>
 </template>
@@ -26,7 +20,7 @@
 <script>
 import { mapState } from 'vuex';
 import BackBar from 'components/BackBar';
- 
+
 export default {
 	name: 'errors',
 
@@ -35,19 +29,48 @@ export default {
 	computed: {
 		errors() {
 			return this.$store.state.page.invalidBlocks;
-		}, 
+		},
 
 		blocks() {
 			return this.$store.state.page.pageData.blocks;
 		},
 
 		region() {
-			return this.$store.state.page.currentRegion;	
+			return this.$store.state.page.currentRegion;
 		}
 	},
 
 	components: {
 		BackBar
 	},
+
+
+	methods: {
+
+		/**
+		@TODO - implement smooth scrolling?
+		scrollTo() - simple jump to the specified block in the iframe 'editor-content'
+		- note that blocks are identified by their block position and not unique api id.
+		- this means as blocks are reordered on the page, the error markers automatically reposition accordingly
+		*/
+		scrollTo(block_id) {
+			var el = document.getElementById('editor-content');
+			var block = el.contentWindow.document.getElementById('block_' + block_id);
+			// position of the block in the iframe
+			var pos = block.getBoundingClientRect();
+			// add on Y scroll position to pos.top to make sure the position for the next jump is relative to the current scroll position
+			el.contentWindow.scrollTo(0, pos.top + el.contentWindow.scrollY);
+		},
+
+		// return the correct human-readable label for the specified block definition
+		label: function(name) {
+			for (var block in this.$store.state.definition.blockDefinitions) {
+				if (block == name) {
+					return this.$store.state.definition.blockDefinitions[block].label;
+				}
+			}
+		}
+	}
+
 };
 </script>
