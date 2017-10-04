161ead5153d02f68ee212c80c3312c84992f2321
Jack <jb2034@kent.ac.uk>
2017-10-04 14:17:51 +0100

    Add comments to undo stack class, might help others understand what it's doing better.

--- resources/assets/js/classes/UndoStack.js
+++ resources/assets/js/classes/UndoStack.js
@@ -10,6 +10,16 @@ export default class UndoStack {
 		callback: () => {}
 	};
 
+	/**
+	 * Create a stack of changes and set up options.
+	 *
+	 * @param {Object}   opts - The options to merge with the defaults.
+	 * @param {number}   opts.maxSize
+	 * @param {boolean}  opts.lock
+	 * @param {number}   opts.undoGroupingInterval
+	 * @param {function} opts.onUndoRedo
+	 * @param {function} opts.callback
+	 */
 	constructor(opts = {}) {
 		opts = { ...this.defaultOptions, ...opts };
 		this.stack = [];
@@ -23,6 +33,14 @@ export default class UndoStack {
 		this.callback = opts.callback;
 	}
 
+	/**
+	 * Runs the callback supplied in the inital options.
+	 *
+	 * @param      {string}  type    The type of action performed (for
+	 * better debugging/feedback), currently add|undo|redo.
+	 *
+	 * @return     {Object}  "this" for method chaining.
+	 */
 	runCallback(type) {
 		this.callback({
 			type,
@@ -34,29 +52,58 @@ export default class UndoStack {
 		return this;
 	}
 
+	/**
+	 * Set lock flag for undo/redo.
+	 *
+	 * @param      {boolean}  lock    Whether to lock or unlock.
+	 *
+	 * @return     {Object}  "this" for method chaining.
+	 */
 	lock(lock = true) {
 		this.isLocked = lock;
 		return this;
 	}
 
+	/**
+	 * Alias for unlocking undo/redo
+	 */
 	unlock() {
 		return this.lock(false);
 	}
 
+	/**
+	 * Set function to run on undo/redo.
+	 *
+	 * @return     {Object}  "this" for method chaining.
+	 */
 	setUndoRedo(func) {
 		this.onUndoRedo = func;
 		return this;
 	}
 
+	/**
+	 * Set the callback to run on every action.
+	 *
+	 * @return     {Object}  "this" for method chaining.
+	 */
 	setCallback(func) {
 		this.callback = func;
 		return this;
 	}
 
+	/**
+	 * Shortcut to add initial data and unlock undo/redo.
+	 */
 	init(data) {
 		return this.add(data).unlock();
 	}
 
+	/**
+	 * Add data to the stack, and group changes based on grouping interval.
+	 * Then run the callback with the "add" action type.
+	 *
+	 * @return     {Object}  "this" for method chaining.
+	 */
 	add(data) {
 		if(this.timer) {
 			clearTimeout(this.timer);
@@ -75,12 +122,20 @@ export default class UndoStack {
 
 			this.index = this.stack.length - 1;
 
+			// run our callback and pass "add" as action type
 			this.runCallback('add');
+
 		}, this.undoGroupingInterval);
 
 		return this;
 	}
 
+	/**
+	 * Travel backwards or forwards through stack.
+	 * Calls our undo/redo function and the generic callback.
+	 *
+	 * @return     {Object}  "this" for method chaining.
+	 */
 	undoOrRedo(type) {
 		const
 			isUndo = type === 'undo',
@@ -100,24 +155,45 @@ export default class UndoStack {
 		return this;
 	}
 
+	/**
+	 * Undo latest change.
+	 */
 	undo() {
 		return this.undoOrRedo('undo');
 	}
 
+	/**
+	 * Redo previous change.
+	 */
 	redo() {
 		return this.undoOrRedo('redo');
 	}
 
+	/**
+	 * Clear the whole undo stack and reset current index.
+	 *
+	 * @return     {Object}  "this" for method chaining.
+	 */
 	clear() {
 		this.stack = [];
 		this.index = -1;
 		return this;
 	}
 
+	/**
+	 * See if there is anything to undo and the stack isn't locked.
+	 *
+	 * @return     {boolean}  If we can undo changes.
+	 */
 	canUndo() {
 		return !this.isLocked && this.index > 0;
 	}
 
+	/**
+	 * See if there is anything to redo and the stack isn't locked.
+	 *
+	 * @return     {boolean}  If we can redo changes.
+	 */
 	canRedo() {
 		return !this.isLocked && this.index < this.stack.length - 1;
 	}
