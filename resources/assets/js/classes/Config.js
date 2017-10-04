37bcc1a4fbb599406faf1087d25d4d737408edcd
Jack <jb2034@kent.ac.uk>
2017-10-04 14:14:55 +0100

    Make config class a little more robust.

--- resources/assets/js/classes/Config.js
+++ resources/assets/js/classes/Config.js
@@ -3,21 +3,42 @@ export default class Config {
 	static options = {};
 
 	static init(options) {
-		Config.options = options;
+		if(
+			typeof options !== 'object' ||
+			options === null ||
+			Array.isArray(options)
+		) {
+			throw Error(
+				'Config #init(options: object) must be supplied with an object'
+			);
+		}
+
+		// create a shallow copy of object
+		// (we are no longer directly referencing the "options"
+		// object but still reference child objects)
+		Config.options = { ...options };
 	}
 
 	static get(key, fallback = null) {
-		return Config.options[key] || fallback;
+		if(Config.options[key] === void 0) {
+			return fallback;
+		}
+
+		return Config.options[key];
 	}
 
 	static set(key, value) {
-		Config.options[key] = value;
+		return Config.options[key] = value;
 	}
 
 	static remove(key) {
-		if(Config.options[key]) {
+		if(Config.options[key] !== void 0) {
 			delete Config.options[key];
 		}
+	}
+
+	static reset() {
+		Config.options = {};
 	}
 
 }
