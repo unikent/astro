6f9027e65ca6b642e5a7540dfa117ca2cc8c8fef
Jack <jb2034@kent.ac.uk>
2017-10-06 17:05:33 +0100

    Add component for displaying and selecting page links from the currently selected site in our page editor.

---
+++ resources/assets/js/components/menu-editor/SitePageLinks.vue
@@ -0,0 +1,29 @@
+<template>
+<el-card>
+	<div slot="header" class="card__header">
+		<span class="card__header-text">Your pages</span>
+	</div>
+	<ul class="menu-page-list">
+		<li
+			v-for="(page, index) in sitePages"
+			class="menu-page-list__page"
+		>
+			<div style="display: flex; flex-direction: column;">
+				<span>{{ page.title }}</span>
+				<span style="font-size: 11px; color: #71787f; margin-top: 4px;">{{ page.path }}</span>
+			</div>
+
+			<el-button @click="onClick({ text: page.title, url: page.path})" size="small" style="margin-left: auto;">
+				Add to menu
+			</el-button>
+		</li>
+	</ul>
+</el-card>
+</template>
+
+<script>
+export default {
+	name: 'site-page-links',
+	props: ['onClick', 'sitePages']
+};
+</script>
