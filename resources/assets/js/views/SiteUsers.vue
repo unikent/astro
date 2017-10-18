f6087689be453d1a0364f91935d5369b00cea1fb
Justice Addison <jay@tisqo.com>
2017-10-18 12:57:45 +0100

    update ui and add ui for adding users

--- resources/assets/js/views/SiteUsers.vue
+++ resources/assets/js/views/SiteUsers.vue
@@ -5,9 +5,45 @@
 			<span class="card__header-text">
 				{{ this.siteTitle }} site users
 			</span>
-			<el-button @click="addUser" type="primary" class="u-flex-auto-left">Add a new user</el-button>
 		</div>
 
+		<h2>Add new users to {{ this.siteTitle }}</h2>
+		<div style="display: flex; padding: 10px; margin-bottom: 40px; background-color: #f7f9fb;">
+
+			<el-select
+				v-model="usersToAdd"
+				multiple
+				filterable
+				placeholder="Search for a user" style="width: 70%">
+				<el-option
+				v-for="user in userList"
+				:key="user.user.id"
+				:label="`${user.user.name} @ ${user.user.username}`"
+				:value="user.user.id">
+				</el-option>
+			</el-select>
+
+			<el-select v-model="selectedRole" placeholder="Select role" class="u-flex-auto-left">
+				<el-option
+					label="Site Owner"
+					value="Site Owner"
+				/>
+				<el-option
+					label="Editor"
+					value="Editor"
+				/>
+				<el-option
+					label="Contributor"
+					value="Contributor"
+				/>
+			</el-select>
+
+			<el-button @click="addUser" type="primary" class="u-flex-auto-left">Add a new user</el-button>
+			
+		</div>
+
+
+		<h2>Existing users</h2>
 		<div style="display: flex; margin-bottom: 20px;">
 			<el-input
 				v-model="searchInput"
@@ -88,7 +124,22 @@
 						</td>
 						<td>
 							<div class="cell">
-								<el-tag type="gray">{{ user.role }}</el-tag>
+								<el-select v-model="user.role" size="small" class="u-flex-auto-left">
+									<el-option-group label="Change role">
+										<el-option
+											label="Site Owner"
+											value="Site Owner"
+										/>
+										<el-option
+											label="Editor"
+											value="Editor"
+										/>
+										<el-option
+											label="Contributor"
+											value="Contributor"
+										/>
+									</el-option-group>
+								</el-select>
 							</div>
 						</td>
 						<td>
@@ -98,15 +149,9 @@
 						</td>
 						<td>
 							<div class="cell">
-							<el-dropdown trigger="click">
-								<el-button size="small">
-									Options <i class="el-icon-caret-bottom el-icon--right"></i>
-								</el-button>
-								<el-dropdown-menu slot="dropdown">
-									<el-dropdown-item>Change role</el-dropdown-item>
-									<el-dropdown-item>Remove from site</el-dropdown-item>
-								</el-dropdown-menu>
-							</el-dropdown>
+							<el-button @click="removeUser(user.id)" type="default" size="small">
+								<icon name="delete" width="14" height="14" />
+							</el-button>
 							</div>
 						</td>
 					</tr>
@@ -115,9 +160,6 @@
 		</div>
 		<div v-else class="site-users__user--empty">
 			No users
-		</div>
-		<div class="site-users__footer">
-			<el-button @click="addUser" type="primary" class="u-flex-auto-left">Add a new user</el-button>
 		</div>
 	</el-card>
 
@@ -140,7 +182,10 @@
 </template>
 
 <script>
+import Icon from 'components/Icon';
+
 export default {
+
 	name: 'site-users',
 
 	props: {
@@ -148,6 +193,10 @@ export default {
 			type: Array,
 			default: () => [20, 50, 100, 200]
 		}
+	},
+
+	components: {
+		Icon
 	},
 
 	created() {
@@ -168,7 +217,192 @@ export default {
 
 			currentPage: 1,
 			count: 20,
-			loading: true
+			loading: true,
+
+			userList: [
+				{
+    "user": {
+      "name": "Howard Reilly",
+      "email": "howardreilly@quintity.com",
+      "username": "user1",
+      "id": 1
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Pugh Bradshaw",
+      "email": "pughbradshaw@quintity.com",
+      "username": "user2",
+      "id": 2
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Lillian Allen",
+      "email": "lillianallen@quintity.com",
+      "username": "user3",
+      "id": 3
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Puckett Jefferson",
+      "email": "puckettjefferson@quintity.com",
+      "username": "user4",
+      "id": 4
+    },
+    "role": "Editor"
+  },
+  {
+    "user": {
+      "name": "French Foreman",
+      "email": "frenchforeman@quintity.com",
+      "username": "user5",
+      "id": 5
+    },
+    "role": "Contributor"
+  },
+  {
+    "user": {
+      "name": "Valerie Arnold",
+      "email": "valeriearnold@quintity.com",
+      "username": "user6",
+      "id": 6
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Gabrielle Henderson",
+      "email": "gabriellehenderson@quintity.com",
+      "username": "user7",
+      "id": 7
+    },
+    "role": "Editor"
+  },
+  {
+    "user": {
+      "name": "Nielsen Morse",
+      "email": "nielsenmorse@quintity.com",
+      "username": "user8",
+      "id": 8
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Adeline Craig",
+      "email": "adelinecraig@quintity.com",
+      "username": "user9",
+      "id": 9
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Ethel Lawson",
+      "email": "ethellawson@quintity.com",
+      "username": "user10",
+      "id": 10
+    },
+    "role": "Contributor"
+  },
+  {
+    "user": {
+      "name": "Janna Horn",
+      "email": "jannahorn@quintity.com",
+      "username": "user11",
+      "id": 11
+    },
+    "role": "Contributor"
+  },
+  {
+    "user": {
+      "name": "Yvonne Pickett",
+      "email": "yvonnepickett@quintity.com",
+      "username": "user12",
+      "id": 12
+    },
+    "role": "Editor"
+  },
+  {
+    "user": {
+      "name": "Hoffman Murray",
+      "email": "hoffmanmurray@quintity.com",
+      "username": "user13",
+      "id": 13
+    },
+    "role": "Editor"
+  },
+  {
+    "user": {
+      "name": "Karen Meyer",
+      "email": "karenmeyer@quintity.com",
+      "username": "user14",
+      "id": 14
+    },
+    "role": "Editor"
+  },
+  {
+    "user": {
+      "name": "Conrad Bates",
+      "email": "conradbates@quintity.com",
+      "username": "user15",
+      "id": 15
+    },
+    "role": "Editor"
+  },
+  {
+    "user": {
+      "name": "Kemp Levine",
+      "email": "kemplevine@quintity.com",
+      "username": "user16",
+      "id": 16
+    },
+    "role": "Contributor"
+  },
+  {
+    "user": {
+      "name": "Esther Richards",
+      "email": "estherrichards@quintity.com",
+      "username": "user17",
+      "id": 17
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Ratliff Henry",
+      "email": "ratliffhenry@quintity.com",
+      "username": "user18",
+      "id": 18
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Ramsey Huber",
+      "email": "ramseyhuber@quintity.com",
+      "username": "user19",
+      "id": 19
+    },
+    "role": "Site Owner"
+  },
+  {
+    "user": {
+      "name": "Guzman Britt",
+      "email": "guzmanbritt@quintity.com",
+      "username": "user20",
+      "id": 20
+    },
+    "role": "Site Owner"
+  }
+			],
+			usersToAdd: [],
+			selectedRole: ''
 		};
 	},
 
