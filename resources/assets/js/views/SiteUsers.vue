4f399748f7753301ce259e2bc6586b9fa92078ac
Justice Addison <jay@tisqo.com>
2017-10-18 15:00:36 +0100

    remove dummy data + some nicities

--- resources/assets/js/views/SiteUsers.vue
+++ resources/assets/js/views/SiteUsers.vue
@@ -38,7 +38,7 @@
 				/>
 			</el-select>
 
-			<el-button @click="addUser" type="primary" class="u-flex-auto-left">Add a new user</el-button>
+			<el-button @click="addUser" type="primary" class="u-flex-auto-left">Add user{{ multipleUsersToAdd ? 's' : ''}}</el-button>
 			
 		</div>
 
@@ -219,188 +219,7 @@ export default {
 			count: 20,
 			loading: true,
 
-			userList: [
-				{
-    "user": {
-      "name": "Howard Reilly",
-      "email": "howardreilly@quintity.com",
-      "username": "user1",
-      "id": 1
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Pugh Bradshaw",
-      "email": "pughbradshaw@quintity.com",
-      "username": "user2",
-      "id": 2
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Lillian Allen",
-      "email": "lillianallen@quintity.com",
-      "username": "user3",
-      "id": 3
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Puckett Jefferson",
-      "email": "puckettjefferson@quintity.com",
-      "username": "user4",
-      "id": 4
-    },
-    "role": "Editor"
-  },
-  {
-    "user": {
-      "name": "French Foreman",
-      "email": "frenchforeman@quintity.com",
-      "username": "user5",
-      "id": 5
-    },
-    "role": "Contributor"
-  },
-  {
-    "user": {
-      "name": "Valerie Arnold",
-      "email": "valeriearnold@quintity.com",
-      "username": "user6",
-      "id": 6
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Gabrielle Henderson",
-      "email": "gabriellehenderson@quintity.com",
-      "username": "user7",
-      "id": 7
-    },
-    "role": "Editor"
-  },
-  {
-    "user": {
-      "name": "Nielsen Morse",
-      "email": "nielsenmorse@quintity.com",
-      "username": "user8",
-      "id": 8
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Adeline Craig",
-      "email": "adelinecraig@quintity.com",
-      "username": "user9",
-      "id": 9
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Ethel Lawson",
-      "email": "ethellawson@quintity.com",
-      "username": "user10",
-      "id": 10
-    },
-    "role": "Contributor"
-  },
-  {
-    "user": {
-      "name": "Janna Horn",
-      "email": "jannahorn@quintity.com",
-      "username": "user11",
-      "id": 11
-    },
-    "role": "Contributor"
-  },
-  {
-    "user": {
-      "name": "Yvonne Pickett",
-      "email": "yvonnepickett@quintity.com",
-      "username": "user12",
-      "id": 12
-    },
-    "role": "Editor"
-  },
-  {
-    "user": {
-      "name": "Hoffman Murray",
-      "email": "hoffmanmurray@quintity.com",
-      "username": "user13",
-      "id": 13
-    },
-    "role": "Editor"
-  },
-  {
-    "user": {
-      "name": "Karen Meyer",
-      "email": "karenmeyer@quintity.com",
-      "username": "user14",
-      "id": 14
-    },
-    "role": "Editor"
-  },
-  {
-    "user": {
-      "name": "Conrad Bates",
-      "email": "conradbates@quintity.com",
-      "username": "user15",
-      "id": 15
-    },
-    "role": "Editor"
-  },
-  {
-    "user": {
-      "name": "Kemp Levine",
-      "email": "kemplevine@quintity.com",
-      "username": "user16",
-      "id": 16
-    },
-    "role": "Contributor"
-  },
-  {
-    "user": {
-      "name": "Esther Richards",
-      "email": "estherrichards@quintity.com",
-      "username": "user17",
-      "id": 17
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Ratliff Henry",
-      "email": "ratliffhenry@quintity.com",
-      "username": "user18",
-      "id": 18
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Ramsey Huber",
-      "email": "ramseyhuber@quintity.com",
-      "username": "user19",
-      "id": 19
-    },
-    "role": "Site Owner"
-  },
-  {
-    "user": {
-      "name": "Guzman Britt",
-      "email": "guzmanbritt@quintity.com",
-      "username": "user20",
-      "id": 20
-    },
-    "role": "Site Owner"
-  }
-			],
+			userList: [],
 			usersToAdd: [],
 			selectedRole: ''
 		};
@@ -423,6 +242,10 @@ export default {
 					) :
 					users
 			);
+		},
+
+		multipleUsersToAdd() {
+			return this.usersToAdd.length > 1;
 		},
 
 		pagedUsers() {
@@ -467,7 +290,7 @@ export default {
 
 		createFilter(searchKeys, searchTerm) {
 			return user => searchKeys.some(
-				key => (user.user[key].indexOf(searchTerm) > -1)
+				key => (user.user[key].indexOf(searchTerm) !== -1)
 			);
 		},
 
