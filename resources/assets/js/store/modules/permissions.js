96040cccc246bb5c987676113842dbf723aa73bf
Christian Cable <c.f.cable@kent.ac.uk>
2017-10-20 16:29:04 +0100

    cleaning up vuex permissions / roles store

--- resources/assets/js/store/modules/permissions.js
+++ resources/assets/js/store/modules/permissions.js
@@ -3,41 +3,30 @@ import api from 'plugins/http/api';
 /**
  * Simple interface for interacting with user roles and permissions
  * 
- * permissions is an object with the following structure
+ * permissions is an array with the following structure
  * 
- * permissions {
- * 	'<permission name>' : [<'role name', 'role name' ...]
- * 
- * }
+[
+        {
+            "name": "Create Subsites",
+            "slug": "subsite.create",
+            "roles": [
+                "site.owner"
+            ]
+        },
+		...
  */
 const state = {
-	permissions: {}
+	roles: [],
+	currentRole: {}
 };
 
 const getters = {
-	/**
-	 * checks if the current user has a specific permission
-	 * the role of the current user has with the current site is assumed to be in the state as role
-	 * 
-	 * @param {object} state - the current vuex state - automagically added
-	 * @param {string} permission_name - the name of a permission e.g. 'editSite', 'addSite' etc
-	 * @returns {boolean} - true if the role has the permission_name
-	 */
-	userCan: (state, getters) => (permission_name) => {
-		if (state.permissions.hasOwnProperty(permission_name)) {
-			return state.permissions[permission_name].includes(state.role);
-		} 
-		else {
-			return false;
-		}
+	getCurrentRole(state, getters) {
+		return state.currentRole;
 	},
 
-	/**
-	 * returns the role that the logged in user has in the current site
-	 * if we do not have a current site then it returns null
-	 */
-	role: (state, getters) => () => {
-
+	getRoles(state, getters) {
+		return state.roles;
 	}
 };
 
@@ -45,10 +34,12 @@ const actions = {
 
 	/**
 	 * gets list of roles from the api can calls mutation to store this in the state
+	 * 
+	 * called from TopBar.vue->mounted
 	 */
 	loadPermissions({commit, state}) {		
 		api
-			.get(`permissions`)
+			.get('permissions?include=roles')
 			.then((response) => {
 				let permissions = response.data.data;
 				commit('setPermissions', permissions);
@@ -57,38 +48,27 @@ const actions = {
 
 	/**
 	 * gets list of a user's roles from the api can calls mutation to store this in the state
+	 * 
+	 * called from Editor.vue->created
 	 */
-	loadUserRoles() {
-
+	loadSitePermissions({commit, state}, site_id) {
+		api
+			.get(`sites/${site_id}?include=users`)
+			.then(({data}) => {
+				const userList = data.data.users;
+				if (userList) {
+					const currentRole = userList.find((element) => element.name === window.astro.username);
+					commit('setCurrentRole', currentRole);
+				}
+				else {
+					commit('setCurrentRole', {});
+				}
+			});
 	}
 
 };
 
 const mutations = {
-
-	/** REPLACE WITH PAYLOAD
-	 * sets the role for a user within a given site
-	 * @param	{string}		username eg. 'cfc7' 
-	 * @param	{array}			array of users for a given site as returned by /api/v1/sites/1?include=users
-	 * @return	{string|null}	the rolename e.g. 'Admin', 'Editor' etc or null is the user has no role
-	 */
-	setRole(state, user_name, site_roles) {
-
-		role = null;
-
-		if (!Array.isArray(site_roles)) {
-			roll = null
-		} else {
-			currentRole = site_roles.find(user => user.user.username === user_name);
-			if (currentRole) {
-				roll =  currentRole.role;
-			} else {
-				roll = null;
-			}
-		}
-
-		state.role = roll;
-	},
 
 	/**
 	 * Used to initially set the permissions the editor understands
@@ -97,10 +77,17 @@ const mutations = {
 	 * @param {object} permissions - the set of permissions as received from the API
 	 */
 	setPermissions(state, permissions) {
-		console.log(permissions);
-		state.permissions = permissions;
+		state.roles = permissions;
+	},
+
+	/**
+	 * sets the current role for the site
+	 * @param {*} state 
+	 * @param {*} currentRole 
+	 */
+	setCurrentRole(state, currentRole) {
+		state.currentRole = currentRole;
 	}
-	
 };
 
 export default {
