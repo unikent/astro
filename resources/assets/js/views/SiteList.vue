d4f9ecf549e19d5804eb3014a31e702ed3e527b8
Justice Addison <jay@tisqo.com>
2017-10-24 11:55:54 +0100

    change tab order for add site form

--- resources/assets/js/views/SiteList.vue
+++ resources/assets/js/views/SiteList.vue
@@ -75,16 +75,12 @@
 		<el-dialog title="Add Site" v-model="dialogFormVisible">
 			<el-form :model="form" label-position="top">
 				<el-row type="flex" :gutter="20">
+
 					<el-col :span="11">
 
 						<el-form-item label="Name">
 							<el-input v-model="form.name" auto-complete="off"></el-input>
 						</el-form-item>
-
-						<el-form-item label="Host">
-							<el-input v-model="form.host" auto-complete="off" placeholder="www.kent.ac.uk"></el-input>
-						</el-form-item>
-
 					</el-col>
 
 					<el-col :span="11" :offset="2">
@@ -93,11 +89,27 @@
 								<el-option v-for="layout in layouts" :label="layout.name" :value="layout" :key="layout.name" />
 							</el-select>
 						</el-form-item>
+					</el-col>
+
+				</el-row>
+
+				<el-row type="flex" :gutter="20">
+
+					<el-col :span="11">
+
+						<el-form-item label="Host">
+							<el-input v-model="form.host" auto-complete="off" placeholder="www.kent.ac.uk"></el-input>
+						</el-form-item>
+
+					</el-col>
+
+					<el-col :span="11" :offset="2">
 
 						<el-form-item label="Path">
 							<el-input v-model="form.path" auto-complete="off" placeholder=""></el-input>
 						</el-form-item>
 					</el-col>
+
 				</el-row>
 
 				<div class="el-alert el-alert--error" v-if="form.errorMsgs">
