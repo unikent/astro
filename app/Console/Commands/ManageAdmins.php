eca4fbc939530c3cb783a50abe83bb83a2914009
Sam Yapp <sam@samyapp.com>
2017-10-27 09:25:16 +0000

    Simple command to list, add and remove admin users. Inlcudes basic search if user not found.

---
+++ app/Console/Commands/ManageAdmins.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Console\Commands;
+
+use ArrayObject;
+use Illuminate\Console\Command;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class ManageAdmins extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'astro:admins
+    							{action? : The action to perform ("list", "+" or "-"). If blank, defaults to "list".}
+    							{username? : The username of the user to add / remove from the list of admins. If blank the user will be prompted to search.}
+                            ';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Manage superadmin users.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $action = $this->argument('action');
+        $username = $this->argument('username');
+        if(!in_array($action, ['+', '-'])){
+        	$action = 'list';
+		}
+		switch( $action ){
+			case '+':
+			case '-':
+				$user = User::query()->where('username', $username)->first();
+				if (!$user) {
+					$this->error('User "' . $username . '" does not exist.');
+					$users = User::query()->where('username', 'like', $username . '%')->get();
+					if($users->count()) {
+						$this->warn('Did you mean?');
+						foreach ($users as $item) {
+							$this->info("\t" . $item->username . ($item->role == 'admin' ? ' ADMIN' : ''));
+						}
+					}
+				}
+				if($user) {
+					$user->role = ('+' == $action ? 'admin' : 'user');
+					$user->save();
+					$this->info('User "' . $username . '" is ' . ('+' == $action ? '' : 'no longer ') . 'an admin.');
+				}
+				break;
+			default:
+				$admins = User::query()->where('role', '=', 'admin')->orderBy('username')->get();
+				$this->alert('There are ' . $admins->count() . ' superadmins.');
+				foreach($admins as $admin){
+					$this->info($admin->username . ' (' . $admin->email . ')');
+				}
+				break;
+		}
+    }
+}
