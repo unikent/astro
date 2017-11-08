500ce6f407cd48510ccd516931689de464c62a4c
Sam Yapp <sam@samyapp.com>
2017-11-08 13:39:20 +0000

    Add sections to blocks and update baked block data in revisions.

---
+++ database/migrations/2017_11_08_132752_add-sections-to-revision_blocks.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Models\Revision;
+
+class AddSectionsToRevisionBlocks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    	$count = $updated = 0;
+		foreach(Revision::all() as $revision){
+			$json = $revision->blocks;
+			$count++;
+			if($json){
+				if(isset($json['main'])
+					&& is_array($json['main'])
+					&& count($json['main']) > 0
+					&& isset($json['main'][0]['definition_name'])) {
+					$section = ['id' => 'catch-all', 'blocks' => $json['main']];
+					$json['main'] = [$section];
+					$revision->blocks = $json;
+					$revision->save();
+					$updated++;
+				}
+			}
+		}
+		echo "Updated $updated of $count revisions.\n";
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
