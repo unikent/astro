50f68301288169a9077efd0529529944b82461c4
Sam Yapp <sam@samyapp.com>
2017-11-10 10:31:57 +0000

    before merging..

--- resources/assets/js/components/Block.vue
+++ resources/assets/js/components/Block.vue
@@ -35,7 +35,7 @@ export default {
 
 	name: 'block',
 
-	props: ['index', 'region', 'type', 'blockData'],
+	props: ['index', 'region', 'type', 'blockData', 'sectionName'],
 
 	data() {
 
@@ -148,19 +148,16 @@ export default {
 			'setBlock',
 			'collapseSidebar',
 			'revealSidebar',
-			'updateMenuActive'
+			'updateMenuActive',
+			'setCurrentBlock'
 		]),
 
 		editBlock() {
-			// set the block in the sidebar if the user clicks on a different block (including one with the same index but in a different region)
-			if(this.currentBlockIndex !== this.index || this.currentRegion !== this.region) {
-				this.collapseSidebar();
-				this.setBlock({ index: this.index, type: this.type });
-			}
-			// make sure we get to see the block menu if we're currently seeing the pages menu and a user clicks on any block
-			if(this.activeMenuItem!=='blocks') {
-				this.updateMenuActive('blocks');
-			}
+			this.dispatch('setCurrentBlock', {
+				regionName: '',
+				sectionName: '',
+				blockPosition: ''
+			});
 		},
 
 		showOverlay() {
