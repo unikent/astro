74abab19402418824628a7467cfdc744f1c14f4d
Justice Addison <jay@tisqo.com>
2017-11-23 10:52:37 +0000

    remove redundant function from BlockBroker, it is now in RegionBroker

--- app/Validation/Brokers/BlockBroker.php
+++ app/Validation/Brokers/BlockBroker.php
@@ -85,7 +85,7 @@ class BlockBroker extends DefinitionBroker
 
 	/**
 	 * Creates a validation rule based on Region block-constraints
-	 * @Todo update this to reflect new structure
+	 * @Todo is this needed?
 	 *
 	 * @param \App\Models\Definition\Region $region
 	 * @return Array
@@ -97,64 +97,6 @@ class BlockBroker extends DefinitionBroker
 				'in:' . implode(',', $region->blocks),
 			]
 		];
-	}
-
-	/**
-	 * Creates an array of validation rules based on Section block-constraints
-	 * NOTE: this return value is not a ruleset but an array of rules to be assembled into 
-	 * a laravel validation ruleset elsewhere
-	 * 
-	 * @param \App\Models\Definition\Region $region
-	 * @param String $section_name
-	 * @return Array [
-	 *				"allowedBlocks" => 'rules for which blocks are allowed',
-	 *				"blockLimits" => 'rules for the amount ob blocks permitted'
-	 *			]
-	 */
-	public function getSectionConstraintRules(RegionDefinition $region, $section_name)
-	{
-		foreach ($region->sections as $section_definition) {
-			if ($section_definition['name'] === $section_name) {
-				
-				$rules = [];
-
-				if (isset($section_definition['allowedBlocks'])) {
-					$rules['allowedBlocks'] = [
-						'definition_name' => [
-							'in:' . implode(',', $section_definition['allowedBlocks']),
-						]
-					];
-				}
-
-				$block_limits = [];
-
-				if (isset($section_definition['optional']) && $section_definition['optional'] ===false) {
-					$block_limits[] = 'required';
-				}
-
-				if (isset($section_definition['size'])) {
-					$block_limits[] = 'size:'.$section_definition['size'];
-				}
-				else{
-
-					if (isset($section_definition['min'])) {
-						$block_limits[] = 'min:'.$section_definition['min'];
-					}
-
-					if (isset($section_definition['max'])) {
-						$block_limits[] = 'max:'.$section_definition['max'];
-					}
-				}
-
-				$rules['blockLimits'] =  [
-					'blocks' => $block_limits;
-				];
-
-				return $rules;
-			}
-		}
-		
-		return [];
 	}
 
 }
