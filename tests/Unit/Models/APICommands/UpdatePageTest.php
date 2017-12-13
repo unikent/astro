000d424c0f43a36c50beb4ad58cb633351035d38
Christian Cable <c.f.cable@kent.ac.uk>
2017-12-13 15:15:05 +0000

    Adding some incomplete tests from the PersistRequestTest tests

--- tests/Unit/Models/APICommands/UpdatePageTest.php
+++ tests/Unit/Models/APICommands/UpdatePageTest.php
@@ -13,117 +13,163 @@ use App\Models\Contracts\APICommand;
 
 class UpdatePageTest extends APICommandTestCase
 {
-    public function getValidData()
-    {
-        return [];
-    }
+	public function getValidData()
+	{
+		return [];
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function validation_whenInput_isValid_passes()
-    {
-        $validator = $this->validator($this->input(null));
-        $validator->passes();
-        $this->assertTrue($validator->passes());
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenInput_isValid_passes()
+	{
+		$validator = $this->validator($this->input(null));
+		$validator->passes();
+		$this->assertTrue($validator->passes());
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function validation_whenPageIsNotDraft_fails()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenPageIsNotDraft_fails()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function validation_whenPageDoesNotExist_fails()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenPageDoesNotExist_fails()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function validation_whenPageIsMissingOrNull_fails()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenPageIsMissingOrNull_fails()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function validation_whenOptionsIsPresentButNotArray_fails()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenOptionsIsPresentButNotArray_fails()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function execute_withOptions_onlyCreatesANewRevisionIdenticalToPreviousRevision_exceptForOptionsTimestampsAndTrackedFields()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenTitleIsBlank_fails()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function execute_withOptions_onlyModifiesOptions_inOptionsArray()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenTitleIsOver150Characters_fails()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function execute_withOptions_removesOptionsWhereNull()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenTitleIs150Characters_passes()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function execute_withTitle_onlyCreatesANewRevisionIdenticalToPreviousRevision_exceptForTitleTimestampsAndTrackedFields()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function validation_whenTitleIsUnder150Characters_passes()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function execute_withNoActualChangesToData_doesNotCreateANewRevision()
-    {
-        $this->markTestIncomplete();
-    }
 
-    /**
-     * @test
-     * @group APICommands
-     */
-    public function execute_returnsPageThatWasModified()
-    {
-        $this->markTestIncomplete();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function execute_withOptions_onlyCreatesANewRevisionIdenticalToPreviousRevision_exceptForOptionsTimestampsAndTrackedFields()
+	{
+		$this->markTestIncomplete();
+	}
 
-    /**
-     * @return APICommand A new instance of the class to test.
-     */
-    public function command()
-    {
-        return new UpdatePage();
-    }
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function execute_withOptions_onlyModifiesOptions_inOptionsArray()
+	{
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function execute_withOptions_removesOptionsWhereNull()
+	{
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function execute_withTitle_onlyCreatesANewRevisionIdenticalToPreviousRevision_exceptForTitleTimestampsAndTrackedFields()
+	{
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function execute_withNoActualChangesToData_doesNotCreateANewRevision()
+	{
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function execute_returnsPageThatWasModified()
+	{
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * @test
+	 * @group APICommands
+	 */
+	public function execute_withoutTitle_createsNewRevisionWithPreviousTitle()
+	{
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * @return APICommand A new instance of the class to test.
+	 */
+	public function command()
+	{
+		return new UpdatePage();
+	}
 }
