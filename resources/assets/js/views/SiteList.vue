fd9fde3bd1b76446092a203d942050a74d36c100
Jack <jb756@kent.ac.uk>
2018-01-23 14:56:10 +0000

    Add button in site listing to go to profile listing page of a site.

--- resources/assets/js/views/SiteList.vue
+++ resources/assets/js/views/SiteList.vue
@@ -84,6 +84,11 @@
 									Users
 								</el-button>
 							</router-link>
+							<router-link :to="`/site/${site.id}/profiles`" v-if="canUserOnSite('profiles.create', site.currentRole)">
+								<el-button type="default" size="small">
+									Profiles
+								</el-button>
+							</router-link>
 							<!-- <el-button @click="askRemove(site.id)" type="default" size="small" v-if="canUserOnSite('site.delete', site.currentRole)">
 								<icon name="delete" width="14" height="14" />
 							</el-button> -->
