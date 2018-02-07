88158328cb90887046bf792a2f936d64d41fa05e
Jack <jb756@kent.ac.uk>
2018-02-07 14:51:56 +0000

    General clean-up and improvements, while we're at it.
    - Make image field extend the block field component.
    - Update collection field so we can reorder items (move up/down), with nice animation.
    - Remove old preset code that was never used.

--- resources/assets/js/components/fields/Collection.vue
+++ resources/assets/js/components/fields/Collection.vue
@@ -1,54 +1,87 @@
 <template>
 <div class="collection-field">
-	<div
-		v-for="(val, index) in currentValue"
-		class="collection-field__field-list"
-	>
-		<template v-for="fieldDefinition in fields">
-			<div class="ci-field">
-				<el-form-item
-					:label="fieldDefinition.label"
-					:prop="`${name}.${index}.${fieldDefinition.name}`"
-					:rules="getRules(fieldDefinition.name)"
-					:error="getError(index, fieldDefinition.name)"
-				>
-					<template slot="label">
-						<span>{{ fieldDefinition.label }}</span>
-						<el-tooltip
-							v-if="fieldDefinition.info"
-							popper-class="el-tooltip__popper--narrow"
-							:content="fieldDefinition.info"
-							placement="top">
-							<icon
-								class="el-form-item__icon-help"
-								name="help-circle"
-								width="15"
-								height="15"
-								viewBox="0 0 15 15"
-							/>
-						</el-tooltip>
-					</template>
-					<component
-						:is="getField(fieldDefinition.type)"
-						:field="childField(fieldDefinition)"
-						:path="`${name}.${index}.${fieldDefinition.name}`"
-						:index="currentIndex"
-						:key="`${name}-${fieldDefinition.name}-${currentIndex}`"
-					/>
-				</el-form-item>
-			</div>
-		</template>
+	<transition-group name="flip-list">
+		<div
+			v-for="(val, index) in currentValue"
+			class="collection-field__field-list"
+			:key="keys[index]"
+		>
+			<template v-for="fieldDefinition in fields">
+				<div class="ci-field">
+					<el-form-item
+						:label="fieldDefinition.label"
+						:prop="`${name}.${index}.${fieldDefinition.name}`"
+						:rules="getRules(fieldDefinition.name)"
+						:error="getError(index, fieldDefinition.name)"
+					>
+						<template slot="label">
+							<span>{{ fieldDefinition.label }}</span>
+							<el-tooltip
+								v-if="fieldDefinition.info"
+								popper-class="el-tooltip__popper--narrow"
+								:content="fieldDefinition.info"
+								placement="top">
+								<icon
+									class="el-form-item__icon-help"
+									name="help-circle"
+									width="15"
+									height="15"
+									viewBox="0 0 15 15"
+								/>
+							</el-tooltip>
+						</template>
+						<component
+							:is="getField(fieldDefinition.type)"
+							:field="childField(fieldDefinition)"
+							:path="`${name}.${index}.${fieldDefinition.name}`"
+							:index="currentIndex"
+							:key="`${name}-${fieldDefinition.name}-${currentIndex}`"
+						/>
+					</el-form-item>
+				</div>
+			</template>
 
-		<div @click="removeItem(index)" class="block-overlay__delete block-overlay__delete--sidebar">
-			<icon name="delete" width="20" height="20" />
+			<div class="collection__modify-buttons">
+				<el-button-group>
+					<el-tooltip :content="`Move this ${friendlyLabel} up`" :open-delay="400">
+						<el-button
+							@click="moveItem(index, -1)"
+							:disabled="index === 0"
+							size="small"
+						>
+							<i class="el-icon-arrow-up el-icon--left"></i>
+						</el-button>
+					</el-tooltip>
+					<el-tooltip :content="`Move this ${friendlyLabel} down`" :open-delay="400">
+						<el-button
+							@click="moveItem(index, 1)"
+							:disabled="index === currentValue.length - 1"
+							size="small"
+						>
+							<i class="el-icon-arrow-down el-icon--right"></i>
+						</el-button>
+					</el-tooltip>
+				</el-button-group>
+
+				<el-tooltip :content="`Remove this ${friendlyLabel}`" :open-delay="400">
+					<el-button
+						plain
+						type="danger"
+						size="small"
+						@click="removeItem(index)"
+					>
+						<icon name="delete" width="15" height="15" />
+					</el-button>
+				</el-tooltip>
+			</div>
 		</div>
-	</div>
+	</transition-group>
 
 	<div class="collection-add">
 		<span class="collection-add__counter">
-			{{ currentValue.length }} {{ this.label.slice(0, -1).toLowerCase() }}{{ currentValue.length === 1 ? '' : 's' }}
+			{{ currentValue.length }} {{ friendlyLabel }}{{ currentValue.length === 1 ? '' : 's' }}
 		</span>
-		<el-button @click="addItem" class="collection-add__button">Add {{ this.label.slice(0, -1).toLowerCase() }}</el-button>
+		<el-button @click="addItem" class="collection-add__button">Add {{ friendlyLabel }}</el-button>
 	</div>
 </div>
 </template>
@@ -75,6 +108,21 @@ export default {
 		Icon
 	},
 
+	created() {
+		let length = this.currentValue.length;
+		this.incr = 0;
+
+		while(length--) {
+			this.keys.push(this.incr++);
+		}
+	},
+
+	data() {
+		return {
+			keys: []
+		};
+	},
+
 	computed: {
 		...mapState({
 			currentIndex: state => state.page.currentBlockIndex
@@ -90,6 +138,10 @@ export default {
 
 		currentValue() {
 			return this.getCurrentFieldValue(this.name);
+		},
+
+		friendlyLabel() {
+			return this.label.slice(0, -1).toLowerCase();
 		}
 	},
 
@@ -119,6 +171,16 @@ export default {
 			});
 
 			value.push(item);
+			this.keys.push(this.incr++);
+
+			this.updateAndValidate(value);
+		},
+
+		moveItem(index, num) {
+			const value = _.cloneDeep(this.getCurrentFieldValue(this.name));
+
+			value.splice(index + num, 0, value.splice(index, 1)[0]);
+			this.keys.splice(index + num, 0, this.keys.splice(index, 1)[0]);
 
 			this.updateAndValidate(value);
 		},
@@ -127,12 +189,13 @@ export default {
 			const value = _.cloneDeep(this.getCurrentFieldValue(this.name));
 
 			value.splice(index, 1);
+			this.keys.splice(index, 1);
 
 			this.updateAndValidate(value);
 		},
 
 		updateAndValidate(value) {
-			this.updateFieldValue({ name: this.name, value });
+			this.updateFieldValue(this.name, value);
 
 			// TODO: change this...
 			// I don't like that I'm forced to use $parent here
