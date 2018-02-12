099401423ad175094047bc0dd351d971b35df664
Jack <jb756@kent.ac.uk>
2018-02-12 09:31:51 +0000

    Move toolbar component into the new folder and split out edit mode switch for reuse.

--- resources/assets/js/components/topbar/Toolbar.vue
+++ resources/assets/js/components/topbar/Toolbar.vue
@@ -1,21 +1,7 @@
 <template>
 <div class="toolbar">
 
-	<el-tooltip v-if="canUser('page.edit')" class="item" effect="dark" content="Switch edit mode" placement="top">
-		<el-select
-			placeholder="view"
-			v-model="view"
-			class="switch-view"
-			:disabled="pageHasLayoutErrors"
-		>
-			<el-option v-for="(view, key) in views" :label="view.label" :value="key" :key="view.label">
-				<div class="view-icon">
-					<icon :name="view.icon" aria-hidden="true" width="20" height="20" />
-				</div>
-				<span class="view-label">{{ view.label }}</span>
-			</el-option>
-		</el-select>
-	</el-tooltip>
+	<switch-mode />
 
 	<el-button
 		v-if="canUser('page.edit')"
@@ -58,6 +44,7 @@
 <script>
 import { mapState, mapMutations, mapActions, mapGetters } from 'vuex';
 
+import SwitchMode from 'components/topbar/SwitchMode';
 import Icon from 'components/Icon';
 import { undoStackInstance } from 'plugins/undo-redo';
 import { win } from 'classes/helpers';
@@ -66,6 +53,7 @@ export default {
 	name: 'toolbar',
 
 	components: {
+		SwitchMode,
 		Icon
 	},
 
@@ -77,14 +65,11 @@ export default {
 
 	computed: {
 		...mapState([
-			'displayIframeOverlay',
-			'undoRedo',
-			'currentView'
+			'undoRedo'
 		]),
 
 		...mapState({
 			page: state => state.page.pageData,
-			pageLoaded: state => state.page.loaded,
 			layoutErrors: state => state.page.layoutErrors
 		}),
 
@@ -93,15 +78,6 @@ export default {
 			'draftPreviewURL',
 			'canUser'
 		]),
-
-		view: {
-			get() {
-				return this.currentView;
-			},
-			set(value) {
-				this.changeView(value);
-			}
-		},
 
 		draftLink() {
 			return this.draftPreviewURL;
@@ -116,32 +92,8 @@ export default {
 		}
 	},
 
-	created() {
-		this.views = {
-			desktop: {
-				icon: 'desktop',
-				label: 'Desktop',
-				width: '100%',
-				height: '100vh'
-			},
-			tablet: {
-				icon: 'tablet',
-				label: 'Tablet',
-				width: '768px',
-				height: '1024px'
-			},
-			mobile: {
-				icon: 'mobile',
-				label: 'Mobile',
-				width: '320px',
-				height: '568px'
-			}
-		};
-	},
-
 	methods: {
 		...mapMutations([
-			'changeView',
 			'showPublishModal',
 			'showPublishValidationWarningModal'
 		]),
