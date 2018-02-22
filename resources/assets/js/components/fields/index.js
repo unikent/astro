d08ffdc8987d50ebd66fdb3c1f095b257b4cefd2
Jack <jb756@kent.ac.uk>
2018-02-22 10:19:22 +0000

    Import display fields and their styles.

--- resources/assets/js/components/fields/index.js
+++ resources/assets/js/components/fields/index.js
@@ -1,9 +1,11 @@
 import baseFields from 'components/fields/base';
 import containerFields from 'components/fields/containers';
+import displayFields from 'components/fields/display';
 import { fields as externalFields } from 'helpers/themeExports';
 
 export default {
 	...baseFields,
 	...containerFields,
+	...displayFields,
 	...externalFields
 };
