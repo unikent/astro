45450cb2d98e3b7817cc2b273ffc8ec87bc02de4
Jack <jb756@kent.ac.uk>
2018-03-09 10:57:11 +0000

    Move link button in rich text toolbar before the list buttons.

--- resources/assets/js/components/richtext/Toolbar.vue
+++ resources/assets/js/components/richtext/Toolbar.vue
@@ -77,18 +77,6 @@ export default {
 				label: 'Sub-heading'
 			},
 			{
-				tag: 'ul',
-				command: 'insertUnorderedList',
-				label: 'Unordered list',
-				icon: 'list'
-			},
-			{
-				tag: 'ol',
-				command: 'insertOrderedList',
-				label: 'Ordered list',
-				icon: 'list-ol'
-			},
-			{
 				tag: 'a',
 				label: 'Link',
 				icon: 'link',
@@ -104,6 +92,18 @@ export default {
 						label: 'Remove link'
 					}
 				]
+			},
+			{
+				tag: 'ul',
+				command: 'insertUnorderedList',
+				label: 'Unordered list',
+				icon: 'list'
+			},
+			{
+				tag: 'ol',
+				command: 'insertOrderedList',
+				label: 'Ordered list',
+				icon: 'list-ol'
 			},
 			{
 				tag: '*',
