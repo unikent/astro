f4723470124f3df34a6d8a9ddafa07843a898ba3
Jack <jb756@kent.ac.uk>
2018-03-27 12:44:45 +0100

    Ensure we can use our h3 icon and that numbers are allowed in icon names.

--- resources/assets/js/components/icons.js
+++ resources/assets/js/components/icons.js
@@ -43,3 +43,4 @@ export { default as site }      from 'IconPath/site.svg';
 export { default as angleup }   from 'IconPath/angle-up.svg';
 export { default as angledown } from 'IconPath/angle-down.svg';
 export { default as write }     from 'IconPath/write.svg';
+export { default as h3 }        from 'IconPath/h3.svg';
