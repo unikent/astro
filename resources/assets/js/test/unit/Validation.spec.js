34cbfcaa5e1d9930fb70e3a05c001497b6435ab9
Jack <jb756@kent.ac.uk>
2018-08-15 18:18:29 +0100

    Make sure any html entities users input are treated as single characters in the client-side code (for the purpose of our validator).

--- resources/assets/js/test/unit/Validation.spec.js
+++ resources/assets/js/test/unit/Validation.spec.js
@@ -111,4 +111,43 @@ describe('Validation transformations', () => {
 		});
 	});
 
+	it('Should treat html entities in "max_length_without_html" as single characters', () => {
+		const
+			entities = [
+				// Printable ASCII (it seems &#32; or the space character has a length of 0 in DOMParser)
+				'#33', '#34', '#35', '#36', '#37', 'amp', '#39', '#40', '#41', '#42', '#43', '#44', '#45', '#46', '#47', '#48', '#49', '#50', '#51', '#52', '#53', '#54', '#55', '#56', '#57', '#58', '#59', 'lt', '#61', 'gt', '#63', '#64', '#65', '#66', '#67', '#68', '#69', '#70', '#71', '#72', '#73', '#74', '#75', '#76', '#77', '#78', '#79', '#80', '#81', '#82', '#83', '#84', '#85', '#86', '#87', '#88', '#89', '#90', '#91', '#92', '#93', '#94', '#95', '#96', '#97', '#98', '#99', '#100', '#101', '#102', '#103', '#104', '#105', '#106', '#107', '#108', '#109', '#110', '#111', '#112', '#113', '#114', '#115', '#116', '#117', '#118', '#119', '#120', '#121', '#122', '#123', '#124', '#125', '#126',
+				// ISO-8859-1
+				'Agrave', 'Aacute', 'Acirc', 'Atilde', 'Auml', 'Aring', 'AElig', 'Ccedil', 'Egrave', 'Eacute', 'Ecirc', 'Euml', 'Igrave', 'Iacute', 'Icirc', 'Iuml', 'ETH', 'Ntilde', 'Ograve', 'Oacute', 'Ocirc', 'Otilde', 'Ouml', 'Oslash', 'Ugrave', 'Uacute', 'Ucirc', 'Uuml', 'Yacute', 'THORN', 'szlig', 'agrave', 'aacute', 'acirc', 'atilde', 'auml', 'aring', 'aelig', 'ccedil', 'egrave', 'eacute', 'ecirc', 'euml', 'igrave', 'iacute', 'icirc', 'iuml', 'eth', 'ntilde', 'ograve', 'oacute', 'ocirc', 'otilde', 'ouml', 'oslash', 'ugrave', 'uacute', 'ucirc', 'uuml', 'yacute', 'thorn', 'yuml',
+				// ISO-8859-1 Symbols
+				'nbsp', 'iexcl', 'cent', 'pound', 'curren', 'yen', 'brvbar', 'sect', 'uml', 'copy', 'ordf', 'laquo', 'not', 'shy', 'reg', 'macr', 'deg', 'plusmn', 'sup2', 'sup3', 'acute', 'micro', 'para', 'cedil', 'sup1', 'ordm', 'raquo', 'frac14', 'frac12', 'frac34', 'iquest', 'times', 'divide',
+				// Math
+				'forall', 'part', 'exist', 'empty', 'nabla', 'isin', 'notin', 'ni', 'prod', 'sum', 'minus', 'lowast', 'radic', 'prop', 'infin', 'ang', 'and', 'or', 'cap', 'cup', 'int', 'there4', 'sim', 'cong', 'asymp', 'ne', 'equiv', 'le', 'ge', 'sub', 'sup', 'nsub', 'sube', 'supe', 'oplus', 'otimes', 'perp', 'sdot',
+				// Greek
+				'Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega', 'alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta', 'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu', 'nu', 'xi', 'omicron', 'pi', 'rho', 'sigmaf', 'sigma', 'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega', 'thetasym', 'upsih', 'piv',
+				// Misc
+				'OElig', 'oelig', 'Scaron', 'scaron', 'Yuml', 'fnof', 'circ', 'tilde', 'ensp', 'emsp', 'thinsp', 'zwnj', 'zwj', 'lrm', 'rlm', 'ndash', 'mdash', 'lsquo', 'rsquo', 'sbquo', 'ldquo', 'rdquo', 'bdquo', 'dagger', 'Dagger', 'bull', 'hellip', 'permil', 'prime', 'Prime', 'lsaquo', 'rsaquo', 'oline', 'euro', 'trade', 'larr', 'uarr', 'rarr', 'darr', 'harr', 'crarr', 'lceil', 'rceil', 'lfloor', 'rfloor', 'loz', 'spades', 'clubs', 'hearts', 'diams'
+			],
+			input = { 'test': `&${entities.join(';&')};` },
+			successful = new Schema({
+				'test' : Validation.transform('max_length_without_html:' + entities.length)
+			}),
+			failing = new Schema({
+				'test' : Validation.transform('max_length_without_html:' + (entities.length - 1))
+			});
+
+		successful.validate(
+			input,
+			errors => {
+				expect(errors).to.be.null;
+			}
+		);
+
+		failing.validate(
+			input,
+			errors => {
+				expect(errors).to.not.be.null;
+			}
+		);
+	});
+
 });
