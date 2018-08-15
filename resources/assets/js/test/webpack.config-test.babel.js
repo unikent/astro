570e804574b080a6239af5024e1cc1faccff5619
Jack <jb756@kent.ac.uk>
2018-08-15 17:26:29 +0100

    Since DOMParser is a browser based API, we need to use the JSDom node package to emulate it in node (mocha CLI) for our tests.
    This also involved adding a node-externals webpack plugin so we don't bundle node modules and use the built-in require instead (fixed errors with jsdom).

--- resources/assets/js/test/webpack.config-test.babel.js
+++ resources/assets/js/test/webpack.config-test.babel.js
@@ -1,8 +1,16 @@
 import webpackConfig from '../../../../webpack.config.babel';
+import nodeExternals from 'webpack-node-externals';
+import { JSDOM } from 'jsdom';
+
+/* global global */
+
+global.DOMParser = new JSDOM().window.DOMParser;
 
 const { module, resolve } = webpackConfig;
 
 export default {
+	target: 'node',
+	externals: [nodeExternals()],
 	module,
 	resolve
 };
