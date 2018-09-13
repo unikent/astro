358bc26913e6e2857d98a6ce302b704dbc4ed1a9
Christian Cable <c.f.cable@kent.ac.uk>
2018-09-13 17:59:47 +0100

    working guard which uses JWT in bearer

--- app/Services/Auth/JwtGuard.php
+++ app/Services/Auth/JwtGuard.php
@@ -2,14 +2,29 @@
 
 namespace App\Services\Auth;
 
+use Config;
+use Exception;
+use App\Models\User;
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Builder;
 use Illuminate\Http\Request;
+use Lcobucci\JWT\ValidationData;
 use Illuminate\Auth\GuardHelpers;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\UserProvider;
+
 
 class JwtGuard implements Guard
 {
     use GuardHelpers;
+
+    /**
+     * Shared key to verify jwt signatures.
+     *
+     * @var string
+     */
+    protected $secretKey;
 
     /**
      * The request instance.
@@ -41,11 +56,41 @@ class JwtGuard implements Guard
      */
     public function __construct(UserProvider $provider, Request $request)
     {
+        // TODO review these values and remove if not used
+        $this->secretKey = Config::get('auth.jwt_secret');
         $this->request = $request;
         $this->provider = $provider;
         $this->inputKey = 'api_token';
         $this->storageKey = 'api_token';
+
     }
+
+
+    /**
+     * retrieves user by JWT
+     * fails if the jwt is invalid or its signature is invalid
+     *
+     * @param string $jwt
+     * @param string $jwt
+     * @return User | false
+     */
+    public function retrieveByJWT($secret, $jwt)
+    {
+
+        $token = (new Parser())->parse((string) $jwt);
+        $data = new ValidationData();
+
+        if (!$token->validate($data)) {
+            throw new Exception("JWT is invalid", 1);
+        }
+
+        if (!$token->verify(new Sha256(), $secret)) {
+            throw new Exception("JWT has invalid signature", 1);
+        };
+
+        return User::where('username', '=', $token->getClaim('uid'))->firstOrFail();
+    }
+
 
     /**
      * Get the currently authenticated user.
@@ -57,7 +102,7 @@ class JwtGuard implements Guard
         // If we've already retrieved the user for the current request we can just
         // return it back immediately. We do not want to fetch the user data on
         // every call to this method because that would be tremendously slow.
-        if (! is_null($this->user)) {
+        if (!is_null($this->user)) {
             return $this->user;
         }
 
@@ -65,10 +110,8 @@ class JwtGuard implements Guard
 
         $token = $this->getTokenForRequest();
 
-        if (! empty($token)) {
-            $user = $this->provider->retrieveByCredentials(
-                [$this->storageKey => $token]
-            );
+        if (!empty($token)) {
+            $user = $this->retrieveByJWT($this->secretKey, $token);
         }
 
         return $this->user = $user;
@@ -81,18 +124,10 @@ class JwtGuard implements Guard
      */
     public function getTokenForRequest()
     {
-        $token = $this->request->query($this->inputKey);
-
-        if (empty($token)) {
-            $token = $this->request->input($this->inputKey);
-        }
+        $token = null;
 
         if (empty($token)) {
             $token = $this->request->bearerToken();
-        }
-
-        if (empty($token)) {
-            $token = $this->request->getPassword();
         }
 
         return $token;
