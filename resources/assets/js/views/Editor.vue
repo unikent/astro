5792c6ae63834f2fb4e21adef44405217a1617db
Christian Cable <c.f.cable@kent.ac.uk>
2018-09-20 17:38:38 +0100

    removing unwanted comments

--- resources/assets/js/views/Editor.vue
+++ resources/assets/js/views/Editor.vue
@@ -54,18 +54,6 @@ export default {
 	},
 
 	created() {
-		// this.$store.commit('site/updateCurrentSiteID', this.$route.params.site_id);
-		// this.$store.dispatch('loadSiteRole', { siteId: this.$route.params.site_id, username: this.username })
-		// // TODO: catch errors
-		// 	.then(() => {
-		// 		if (this.canUser('page.edit')) {
-		// 			this.showLoader();
-		// 		}
-		// 		else {
-		// 			this.showPermissionsError = true;
-		// 		}
-		// 	});
-
 		this.views = {
 			desktop: {
 				icon: 'desktop',
