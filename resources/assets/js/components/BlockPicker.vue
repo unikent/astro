e378f010f3d6d57fcb4df6ca4948a50b6e702399
Matthew Bull <m.bull@kent.ac.uk>
2020-10-07 17:25:26 +0100

    fix to get deprecated as a field in the block

--- resources/assets/js/components/BlockPicker.vue
+++ resources/assets/js/components/BlockPicker.vue
@@ -70,7 +70,7 @@ export default {
 			if(this.allowedBlocks) {
 				for(let i in this.allowedBlocks) {
 					let blockId = this.allowedBlocks[i];
-					if (this.deprecatedBlocks.indexOf(blockId) === -1) {
+					if (!this.allBlocks[blockId].deprecated) {
 						if (this.allBlocks[blockId]) {
 							blocks[blockId] = this.allBlocks[blockId];
 						}
